5874a7bffc7919c6f199dd3c83af42ddd62d5cdc
ashxvegeta <ashutoshroy453@gmail.com>
2025-12-16 22:21:50 +0530

    add route fot role middleware

--- routes/auth.php
+++ routes/auth.php
@@ -11,6 +11,10 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', fn () => 'Admin Area');
+});
+
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
