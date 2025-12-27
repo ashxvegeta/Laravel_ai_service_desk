493e7eee5e2ed43c1125f00ddaf61767c4e8f43f
ashxvegeta <ashutoshroy453@gmail.com>
2025-12-27 15:09:26 +0530

    Update Comment Controller for notification Added

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Comment;
 use App\Models\Ticket;
+use App\Models\User;
+use App\Notifications\TicketCommented;
 
 class CommentController extends Controller
 {
@@ -20,21 +22,22 @@ class CommentController extends Controller
         abort(403);
     }
 
-    Comment::create([
-        'ticket_id' => $ticket->id,
-        'user_id' => auth()->id(),
-        'body' => $request->body,
-    ]);
+     $comment = Comment::create([
+            'ticket_id' => $ticket->id,
+            'user_id'   => auth()->id(),
+            'body'      => $request->body,
+        ]);
 
-    if(auth()->user()->role === 'admin'){
-        // notify ticket owner about new comment
-       $ticket->user->notify(new TicketCommented($ticket, $comment));
-    }else{
-        // notify all admins about new comment
-        User::where('role', 'admin')->each(function($admin) use ($ticket, $comment) {
-            $admin->notify(new TicketCommented($ticket, $comment));
-        });
-    }
+        // notifications
+        if (auth()->user()->role === 'admin') {
+            // Admin replied → notify ticket owner
+            $ticket->user->notify(new TicketCommented($ticket, $comment));
+        } else {
+            // User replied → notify all admins
+            User::where('role', 'admin')->each(function ($admin) use ($ticket, $comment) {
+                $admin->notify(new TicketCommented($ticket, $comment));
+            });
+        }
 
     return redirect()->back()->with('success', 'Comment added successfully.');
 }
