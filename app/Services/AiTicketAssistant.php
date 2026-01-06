7717b95a9656accd84027dffffd42fd0296e2ae6
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-06 23:20:13 +0530

    Added suggest functio to AiTicketAssistant class

--- app/Services/AiTicketAssistant.php
+++ app/Services/AiTicketAssistant.php
@@ -1,14 +1,26 @@
 <?php
 
 namespace App\Services;
+use OpenAI\Laravel\Facades\OpenAI;
+use App\Models\Embedding;
 
 class AiTicketAssistant
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
+   
+    public function suggest(string $question):?string{
+        //generate embedding for the question
+        $queryEmbedding = $this->embed($question);
+        // find relevent knowledge chunks 
+        $relevantChunks = $this->searchRelevant($queryEmbedding);
+        //no knowlwedge found
+        if(empty($relevantChunks)){
+            return null;
+        }
+        //Build Ai context
+        $context = $relevantChunks->pluck('content')->join("\n");
+
+        return  $this->askAi($question, $context);
     }
+
+    
 }
