a00ce26d79b566c38b262375dcfd6132e6de56c2
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-10 13:27:53 +0530

    Created embediing table

---
+++ database/migrations/2026_01_06_184609_create_embeddings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('embeddings', function (Blueprint $table) {
+            $table->id();
+            // Original text chunk
+            $table->text('content');
+            // Vector embedding (stored as JSON)
+            $table->json('embedding');
+              // Optional metadata
+            $table->string('source')->nullable(); // pdf name, faq, manual, etc.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('embeddings');
+    }
+};
