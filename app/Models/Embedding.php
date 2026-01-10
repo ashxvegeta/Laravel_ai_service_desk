93fc715e64aa41e0bdc7f4166e7cc3ec43562701
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-10 13:36:19 +0530

    Added embdediing model

--- app/Models/Embedding.php
+++ app/Models/Embedding.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Embedding extends Model
 {
     //
+    protected $fillable = ['content', 'embedding', 'source'];
+
+    protected $casts = [
+        'embedding' => 'array', // Cast JSON to array
+    ];
 }
