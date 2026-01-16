6b52c307aecea29b6a352e80c4b9a3fe614708c4
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-16 22:14:07 +0530

    Create Listener

---
+++ app/Listeners/SendTicketToAi.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\TicketCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Jobs\GenerateAiSuggestion;
+
+class SendTicketToAi
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(TicketCreated $event): void
+    {
+        //
+        GenerateAiSuggestion::dispatch($event->ticket);
+    }
+}
